79a0d6af3cb34e41d022c1158b8170caffcbc5fa
Michelle Rogals <mrogals1@uvm.edu>
2020-04-03 11:18:55 -0400

    Update mancala.py
    filled in valid moves

--- mancala.py
+++ mancala.py
@@ -1,17 +1,22 @@
 
 
+#### NOTE: this has not been encorporated with Frank's game.py yet- this is just a basic structure
+
 # valid_moves() returns the indexes of the pits that the player can choose from
 # (i.e. the pits on their side that are not empty).
 def valid_moves(board, player):
-    # I'll fill this in soon
-    pass
+    moves = []
+    for i in range(1,7):
+        if board[player][i] != 0:
+            moves.append(i)
+    return moves
 
 
 def main():
-    board = [[4, 4, 4, 4, 4, 4, 0],  # FRANK replace this with whatever board you end up initializing
-             [4, 4, 4, 4, 4, 4, 0]]
+    board = [[0, 4, 4, 4, 4, 4, 4, 0],  # where [0][0] is the store of player 0, [1][7] is store of player 1
+             [0, 4, 4, 4, 4, 4, 4, 0]]  # and [0][7], [1][0] are just placeholders
 
-    possible_moves = [0, 1, 2, 3, 4, 5] #assumes all moves possible at beginning
+    possible_moves = [1, 2, 3, 4, 5, 6] #this is the index of available moves with respect to a given player
     player = 0
 
     while len(possible_moves) != 0:
@@ -20,7 +25,7 @@ def main():
 
             # Do some stuff (get user input, move, ect.)
 
-            if last_stone[1] == 6: #if the last stone ended up in the store, take another turn
+            if last_stone == 0: #if the last stone ended up in the store, take another turn (for computer this would be 7 not 0)
                 player += 1
 
             possible_moves = valid_moves(board, player) #update
